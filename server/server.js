f68697c85464e5bfc6dab78e803895c192452d1a
Jason Weaver <119889739+Jweaver74@users.noreply.github.com>
2023-07-25 19:21:27 -0500

    updated

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,8 +21,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client'))
+})
+
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+})
+})
+};
+
+
+
+
+startApolloServer();
